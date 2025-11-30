f0c9267358f1ef0f34ceb7d63754826dc0b19893
luto <luciomatiasescalante1@gmail.com>
2025-11-30 09:51:30 -0300

    primer commit primer subida del proyecto

---
+++ BlogChacoFaunayFlora/settings/local.py
@@ -0,0 +1,18 @@
+from .base import *
+
+#Todos lo de abajo es de base.py
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-5r7m&ohp$rsbo%u$)ryckka#c!yd_ja(_wn3h1wdt!32se22_7'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+#Esto es de base.py
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
---
+++ BlogChacoFaunayFlora/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def Home(request):
+    return render(request, 'home.html')
+
+def Contacto(request):
+    return render(request, 'contacto.html')
+
+def Nosotros(request):
+    return render(request, 'nosotros.html')
